a38eee96bd873c6336d8f97be3e7f00193410585
Lucas Lima <lucasfsl@al.insper.edu.br>
2023-08-29 07:43:11 -0300

    resoulcao desafio CSS

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 def extract_route(req):
     route = req.split('\n')[0]
@@ -10,11 +11,28 @@ def read_file(path):
     return file.read()
 
 def load_data(file):
-    with open("data/"+file, 'r') as arquivo:
+    caminho = Path() / 'data' / file
+    with open(caminho, 'r') as arquivo:
         dados = json.load(arquivo)
     return dados
 
 def load_template(file):
-    with open("templates/"+file, 'r') as arquivo:
+    caminho = Path() / 'templates' / file
+    with open(caminho, 'r') as arquivo:
         conteudo = arquivo.read()
     return conteudo
+
+def add_in_file(dic):
+    caminho = Path() / 'data' / 'notes.json'
+    with open(caminho,'r') as r:
+        data = json.load(r)
+    data.append(dic)
+    with open(caminho, 'w') as f:
+        json.dump(data,f)
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    httpv = 'HTTP/1.1'
+    response = httpv+' '+str(code)+' '+reason+'\n\n'+body
+    if len(headers)>0:
+        response = httpv+' '+str(code)+' '+reason+'\n'+headers+'\n\n'+body
+    return response.encode()
